b2397162bc308280cb5360add65012b8124d28cd
Lc3586 <2445624023@qq.com>
2023-03-14 10:22:39 +0800

    ~

--- src/main/java/top/lctr/cagc/configures/ServiceConfigure.java
+++ src/main/java/top/lctr/cagc/configures/ServiceConfigure.java
@@ -5,17 +5,19 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PreDestroy;
+
 /**
- * 启动各个服务
+ * 服务配置
  *
  * @author LCTR
  * @date 2023-03-03
  */
 @Component
 @Order(value = 1)
-public class StartService
+public class ServiceConfigure
         implements ApplicationRunner {
-    public StartService() {
+    public ServiceConfigure() {
 
     }
 
@@ -28,5 +30,13 @@ public class StartService
     public void run(ApplicationArguments args) {
 //        CompletableFuture.runAsync(handler::start,
 //                                   Executors.newSingleThreadExecutor());
+    }
+
+    /**
+     * 关闭各个服务
+     */
+    @PreDestroy
+    public void shutDown() {
+
     }
 }
